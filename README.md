1ce72276e11617b0ad557715fd1e527474b90354
beka <mr.begzodjon96@mail.ru>
2020-04-28 23:50:17 +0300

    Readme file modified

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Linter
 
-**In this project, I implement Linter to check code file from source file**
+**In this project, I implement Linter to check source file of the CSS**
 
+>Concept of the Linter
+
+lint, or a linter, is a tool that analyzes source code to flag programming errors, bugs, stylistic errors, and suspicious constructs. The term originates from a Unix utility that examined C language source code.
+
+
+The analysis performed by lint-like tools can also be performed by an optimizing compiler, which aims to generate faster code. In his original 1978 paper, Johnson addressed this issue, concluding that "the general notion of having two programs is a good one" because they concentrated on different things, thereby allowing the programmer to "concentrate at one stage of the programming process solely on the algorithms, data structures, and correctness of the program, and then later retrofit, with the aid of lint, the desirable properties of universality and portability"
+
+Even though modern compilers have evolved to include many of lint's historical functions, lint-like tools have also evolved to detect an even wider variety of suspicious constructs. These include "warnings about syntax errors, uses of undeclared variables, calls to deprecated functions, spacing and formatting conventions, misuse of scope, implicit fallthrough in switch statements, missing license headers, [and]...dangerous language features.
+
+Lint-like tools are especially useful for interpreted languages like JavaScript and Python. Because such languages lack a compiling phase that displays a list of errors prior to execution, the tools can also be used as simple debuggers for common errors (e.g. syntactic discrepancies) as well as hard-to-find errors such as heisenbugs (drawing attention to suspicious code as "possible errors"). Lint-like tools generally perform static analysis of source code.
 
 
 ## Built With
@@ -15,7 +25,7 @@ In order to start with the project:
 1. You need to have ruby environment installed. [Click here for instructions](https://www.ruby-lang.org/en/documentation/installation/)
 2. Clone this repository `https://github.com/Bekhzod96/Linter.git`
 3. Run the algorithm file
-    - In a terminal window write `ruby `*`[your_file_path]`*`ruby_linter.rb`
+    - In a terminal window write `ruby `*`[your_file_path]`*`bin/ruby_linter.rb`
 
 ## Author
 
---
+++ lib/test.rb
@@ -0,0 +1,90 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/PerceivedComplexity
+class Test
+  attr_reader :file, :extension
+  ERROR_BODY = [
+    [/.;/, "Expected newline after ';' in a multi-line declaration block"],
+    [/^\s{2}\w/, 'Expected indentation of 2 spaces'],
+    [/.:\s\w/, "Expected single space after ':' with a single-line declaration"],
+    [/\S;/, "Unexpected whitespace bofore ';'"],
+    [/\S+\n$/, 'Unexpected whitespace at the end of the line']
+  ].freeze
+  BLOCK_HEADER_ERROR = [
+    [/.\s{/, "Expected single space before '{'"],
+    [/.\S\n/, 'Expected new line']
+  ].freeze
+
+  BLOCK_END_ERROR = [
+    [/^\S/, 'Expected indentation of 0 spaces'],
+    [/^\W/, "Expected newline before '}' of a multi-line block"]
+  ].freeze
+
+  ERROR_DESC = [
+    [/^\s*$/, 'Unexpected empty line detected']
+  ].freeze
+
+  def initialize(url)
+    @file = load_file(url)
+    @extension = url.split('.')[-1]
+    @block = false
+  end
+
+  def load_file(url)
+    file = []
+    return 'Enter Valid path for file' unless File.exist? url
+
+    File.foreach(url) { |line| file << line }
+    file
+  end
+
+  def run_test
+    tracker = Hash.new(0)
+    errors = []
+    @file.each_with_index do |line, line_ind|
+      if /[{]/ =~ line
+        BLOCK_HEADER_ERROR .each_with_index do |err, i|
+          line =~ err[0] ? next : errors << "#{line_ind + 1}: #{BLOCK_HEADER_ERROR[i][1]}"
+        end
+        @block = true
+        next
+      elsif /[}]/ =~ line
+
+        @block = false
+        tracker = Hash.new(0)
+        BLOCK_END_ERROR.each_with_index do |err, i|
+          line =~ err[0] ? next : errors << "#{line_ind + 1}: #{BLOCK_END_ERROR[i][1]}"
+        end
+        next
+      elsif @block
+        tracker[line.split(/\W+/)[1]] += 1
+        if ERROR_DESC[0][0] =~ line
+          errors << "#{line_ind}: #{ERROR_DESC[0][1]}"
+          break
+        end
+        ERROR_BODY.each_with_index do |err, i|
+          line =~ err[0] ? next : errors << "#{line_ind + 1}: #{ERROR_BODY[i][1]}"
+        end
+
+      end
+
+      multi = check_multiplication(tracker)
+      multi.empty? ? nil : errors << "#{line_ind}: Unexpected multi declaration of #{multi[0]}"
+    end
+
+    puts errors
+    return '1 files inspected, no offenses detected' if errors.empty?
+  end
+
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/PerceivedComplexity
+
+  def check_multiplication(hash)
+    result = []
+    hash.each do |key, value|
+      result << key if value > 1
+    end
+    result
+  end
+end
